e1fc6cd3c9060087d2caeb25f473c27cc738b7f5
GovardhanSahil <govardhannandimedala.ggktech.com>
2017-06-23 17:50:00 +0530

    Commited

---
+++ BusinessLayer/BusinessAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BusinessLayer
+{
+    public class GetUserData
+    {
+        public bool CheckuserData(string username, string password){
+            if(username=="sahil" && password=="enter")
+            return true;
+            else return false;
+        }
+    }
+}
---
+++ LogInPage/AuthentPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BusinessLayer;
+
+namespace LogInPage
+{
+    public class Authentor
+    {
+        GetUserData verify=new GetUserData();       
+        public void UserLogIn(){
+            bool x;
+            do{
+            Console.WriteLine("Type Username:");
+            string username=Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            string password=Console.ReadLine();
+            if(x=verify.CheckuserData(username, password)){
+                Console.WriteLine("-----WELCOME to LIBRARY-----");
+
+            }
+            else Console.WriteLine("Invalid Login Credentials!");
+            }while(x!=true);
+
+        }
+
+        public void AdminLogIn(){
+            
+            Console.WriteLine("Type Admin username:");
+            Console.ReadLine();
+            Console.WriteLine("Enter Admin Password:");
+            Console.ReadLine();
+
+            Console.WriteLine("ADMIN Params sent to BL");
+        }
+        public void Register(){
+
+
+        }
+    }
+    public class AuthentorSelect{
+        public int CallChoice(int choice){
+            Authentor obj=new Authentor();
+            switch (choice) {
+                    case 1:
+                        obj.UserLogIn();
+                        break;
+                    case 2:
+                        obj.Register();
+                        break;
+                    case 3:
+                        obj.AdminLogIn();
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you!");
+                        return 1;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                return 0;
+
+        }
+
+    }
+}
