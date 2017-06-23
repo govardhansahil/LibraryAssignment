adc44284098abf559cb323d49957225f677b609d
GovardhanSahil <govardhannandimedala.ggktech.com>
2017-06-23 17:49:28 +0530

    Second commit_Single user authenticated

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,34 +1,21 @@
 using System;
+using LogInPage;
 
 namespace LibraryManagement
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public class Program
+    {   
+        public static void Main(string[] args)
         {
-            bool _authenstatus=true;
-            while(_authenstatus){
-                Console.WriteLine("\n\n Select the option from below and click 'ENTER'\n\n"+"1. User Login \n"+"2. Register\n"+"3. Admin Login\n"+"4. Exit\n");
+            AuthentorSelect AS=new AuthentorSelect();
+            int checker;
+            do{
+                Console.WriteLine("Welcome to the Library!");
+                Console.WriteLine("\n\n Select the option from below and click 'ENTER'\n"+"1. User Login \n"+"2. Register\n"+"3. Admin Login\n"+"4. Exit\n");
                 int _uiChoice=int.Parse(Console.ReadLine());
-                switch (_uiChoice) {
-                    case 1:
-                        Console.WriteLine("Selected Login");
-                        break;
-                    case 2:
-                        Console.WriteLine("Selected Registration");
-                        break;
-                    case 3:
-                        Console.WriteLine("Selected Admin login");
-                        break;
-                    case 4:
-                        Console.WriteLine("Thank you!");
-                        _authenstatus=false;
-                         break;
-                    default:
-                        Console.WriteLine("Invalid option");
-                        break;
-                }
-            }
+               checker= AS.CallChoice(_uiChoice);
+                
+            }while(checker!=1);
             Console.ReadKey();
         }
     }
